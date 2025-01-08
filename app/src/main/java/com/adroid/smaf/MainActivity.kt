41286e2774d89e1fb7968e5ac6de09ad319e2e85
bailin633 <2412433138@qq.com>
2025-01-08 23:31:59 +0800

    test

--- app/src/main/java/com/adroid/smaf/MainActivity.kt
+++ app/src/main/java/com/adroid/smaf/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SMAF_AndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android,Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
